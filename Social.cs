adfb2521e64ffd07b86b40b382935a38432e2566
nypnop <68521696+nypnop@users.noreply.github.com>
2021-03-23 00:04:27 +0700

    add BFS and pathBFS

--- Social.cs
+++ Social.cs
@@ -18,6 +18,7 @@ public class Social{
         found = false;
         exploreDFS(ref stack, vertex1, vertex2, isVisited, ref found);
     }
+    
 
     public void exploreDFS(ref List<string> stack, string vertex1, string vertex2, bool[] isVisited, ref bool found){
 
@@ -47,6 +48,83 @@ public class Social{
         }
 
         if(!found) stack.RemoveAt(stack.Count - 1);
+    }
+    
+    public bool BFS(string vertex1, string vertex2, bool[] isVisited, string []pred)
+    {
+        bool found = false;
+        Queue<string> qbfs = new Queue<string>();
+        qbfs.Enqueue(vertex1);
+        int i = network.getindex(vertex1);
+        isVisited[i] = true;
+        for(int j=0; j < network.size; j++)
+        {
+            pred[j] = "-";
+        }
+        while (qbfs.Count > 0 && !found)
+        {
+            vertex1 = qbfs.First();
+            Vertex V = network.getVertex(vertex1);
+            qbfs.Dequeue();
+            if (vertex1 == vertex2)
+            {
+                found = true;
+            }
+            List<string> neighbours = V.getNeighbours();
+            neighbours.Sort();
+            if (!found)
+            {
+                foreach (string neighbour in neighbours)
+                {
+                    int k = network.getindex(neighbour);
+                    if (!isVisited[k])
+                    {
+                        qbfs.Enqueue(neighbour);
+                        pred[k] = vertex1;
+                        isVisited[k] = true;
+                     }
+                }
+            }
+        }
+        if (found)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public void pathBFS(string vertex1, string vertex2)
+    {
+        bool[] isVisited = new bool[network.size];
+        for (int i = 0; i < network.size; i++)
+        {
+            isVisited[i] = false;
+        }
+        string[] pred = new string[network.size];
+        if (BFS(vertex1, vertex2, isVisited, pred) == false)
+        {
+            Console.WriteLine("make a connection");
+        }
+        else
+        {
+            List<string> path = new List<string>();
+            int idxpred = network.getindex(vertex2);
+            path.Add(vertex2);
+            while(pred[idxpred] != "-")
+            {
+            path.Add(pred[idxpred]);
+            string currV = pred[idxpred];
+            idxpred = network.getindex(currV);
+            }
+            path.Reverse();
+            foreach(string s in path)
+            {
+                Console.WriteLine(s);
+            }  
+        }
     }
 
     static void Main(string[] args){
