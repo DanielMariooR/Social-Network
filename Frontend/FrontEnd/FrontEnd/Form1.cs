0b913fb9d35326551eaad669b39e0f2e16f08dbc
DanielMariooR <13519031@std.stei.itb.ac.id>
2021-03-23 08:26:19 +0700

    fix Frontend error

--- Frontend/FrontEnd/FrontEnd/Form1.cs
+++ Frontend/FrontEnd/FrontEnd/Form1.cs
@@ -13,6 +13,13 @@ namespace FrontEnd
 {
     public partial class Form1 : Form
     {
+        public List<string> nodes;
+        public List<Pair> edges;
+        public string vertex1;
+        public string vertex2;
+        public bool isDFS;
+        public bool isBFS;
+
         public Form1()
         {
             InitializeComponent();
@@ -36,18 +43,26 @@ namespace FrontEnd
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if (comboBox2.SelectedItem != null)
+            {
+                this.vertex1 = comboBox2.GetItemText(comboBox2.SelectedItem);
+            }
         }
 
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if (comboBox3.SelectedItem != null)
+            {
+                this.vertex2 = comboBox3.GetItemText(comboBox3.SelectedItem);
+            }
         }
 
         private void Browse_Click(object sender, EventArgs e)
         {
             var fileContent = string.Empty;
             var filePath = string.Empty;
+            int size = 0;
+            int count = 0;
 
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
@@ -67,16 +82,64 @@ namespace FrontEnd
 
                     using (StreamReader reader = new StreamReader(fileStream))
                     {
-                        // Parse the file
-                        // use counter 
-                        // if counter == 0 then change string into integer
-                        // if not then change string into two char
-                        fileContent = reader.ReadToEnd();
+
+                        comboBox2.Items.Clear();
+                        comboBox3.Items.Clear();
+                        comboBox2.ResetText();
+                        comboBox3.ResetText();
+                        this.isBFS = false;
+                        this.isDFS = false;
+
+                        string ln;
+                        edges = new List<Pair>();
+                        nodes = new List<string>();
+
+                        while ((ln = reader.ReadLine()) != null)
+                        {
+                            if (count == 0)
+                            {
+                                size = int.Parse(ln);
+                            }
+                            else
+                            {
+                                string[] temp = ln.Split(' ');
+                                Pair tempPair = new Pair(temp[0], temp[1]);
+                                edges.Add(tempPair);
+
+                                if (!nodes.Contains(temp[0])) nodes.Add(temp[0]);
+                                if (!nodes.Contains(temp[1])) nodes.Add(temp[1]);
+
+                            }
+                            count++;
+                        }
                     }
                 }
             }
 
-            MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+            MessageBox.Show(edges[0].vertex1);
+            if (nodes.Count != 0)
+            {
+                foreach (string s in nodes)
+                {
+                    comboBox2.Items.Add(s);
+                    comboBox3.Items.Add(s);
+                }
+            }
+
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            Microsoft.Msagl.Drawing.Graph graphLayout = new Microsoft.Msagl.Drawing.Graph();
+
+            foreach (Pair P in edges)
+            {
+                graphLayout.AddEdge(P.vertex1, P.vertex2);
+            }
+
+            viewer.Graph = graphLayout;
+            this.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel1.Controls.Add(viewer);
+            this.ResumeLayout();
+
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
@@ -88,5 +151,6 @@ namespace FrontEnd
         {
 
         }
+
     }
 }
---
+++ Frontend/FrontEnd/FrontEnd/Pair.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FrontEnd
+{
+	public class Pair
+	{
+		public string vertex1 { get; set; }
+		public string vertex2 { get; set; }
+
+		public Pair(string v1, string v2)
+		{
+			this.vertex1 = v1;
+			this.vertex2 = v2;
+		}
+	}
+}
